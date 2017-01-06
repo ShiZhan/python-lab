175681fad937a921da64050c405e8b897ccfb12e
ShiZhan <g.shizhan.g@gmail.com>
2017-01-06 11:16:33 +0800

    use prebuilt packages

--- install_python.sh
+++ install_python.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 # script name:     install_python.sh
 
-#Python 3 version to install
-version="3.5.2"
-
-apt-get install -y build-essential libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl wget
-
-wget "https://www.python.org/ftp/python/$version/Python-$version.tgz"
-tar zxvf "Python-$version.tgz"
-cd "Python-$version"
-./configure
-make
-make install
-pip3 install pip --upgrade
-
-# soft link to make pip3 default
-ln -s /usr/local/bin/pip3 /usr/local/bin/pip
-
-# clean up
-cd ..
-rm -rf "./Python-$version"
-rm "./Python-$version.tgz"
+apt-get install -y python3.5 python3.5-dev
+wget https://bootstrap.pypa.io/get-pip.py -O - 2> /dev/null | python3.5
